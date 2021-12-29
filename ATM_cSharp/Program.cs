d0d109fd12fea208f486a33c9310c15995a44aec
Flamcom27 <27flamcom06@gmail.com>
2021-12-29 23:15:38 +0300

    Dict of money is generated; InputSum func is created

--- ATM_cSharp/Program.cs
+++ ATM_cSharp/Program.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ATM_cSharp // Note: actual namespace depends on the project name.
+namespace ATM_cSharp
 {
     public class Program
     {
+        public static Dictionary<int, int> CreateMoneyBank()
+        {
+            List<int> banknotes = new List<int>() { 1, 2, 5, 10, 20, 50, 100 };
+            Dictionary<int, int> moneyDict = new Dictionary<int, int>();
+            foreach (int i in banknotes)
+            {
+                moneyDict.Add(i, 100);
+            }
+            return moneyDict;
+        }
+        public static int InputWithdraw()
+        {
+            int money = Convert.ToInt32(Console.ReadLine());
+            return money;
+        }
         public static void Main(string[] args)
         {
-            Dictionary<int, int> moneyDict = new Dictionary<int, int>();
-            moneyDict.Add(1, 2);
-            moneyDict.Add(2, 6);
-            foreach (int i in moneyDict.Keys)
-            {
-                Console.WriteLine("key:{0} value{1}", i, moneyDict[i]);
-            }
+            Dictionary<int, int> moneyDict = CreateMoneyBank();
+            int Withdraw = InputWithdraw();
         }
     }
 }
