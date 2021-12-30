6f5af6724ebbc4fe88dc95f0b86a1994bb0f555a
Flamcom27 <27flamcom06@gmail.com>
2021-12-30 23:41:27 +0300

    func SortWithdraw is created, an algorithm of calculation is improved

--- ATM_cSharp/Program.cs
+++ ATM_cSharp/Program.cs
@@ -4,54 +4,50 @@ using System.Linq;
 
 namespace ATM_cSharp
 {
+    public class GlobalVars
+    {       
+        public Dictionary<int, int> moneyDict = new Dictionary<int, int>();
+        public int sum;
+        public List<string> withdraw = new List<string>();
+    }
     public class Program
     {
-        public static List<string> CalculateWithdraw(int sum, Dictionary<int, int> dict) 
+        public static void SortWithdraw(GlobalVars GV, int output, int length, List<int> banknotes, int dec)
+        {
+            foreach (int i in banknotes)
+            {
+                output = GV.sum / dec;
+                output *= dec;
+                if (i <= output)
+                {
+                    GV.moneyDict[i] -= output / i;
+                    GV.withdraw.Add(String.Format("{0}$ x {1}", i, output / i));
+                    GV.sum -= output - (output % i);
+                    if (output - i == 0) { break; }
+                    else { continue; }
+                }
+                else { continue; }
+            }
+        }
+        public static void CalculateWithdraw(GlobalVars GV) 
         { 
-            List<string> withdarw = new List<string>();
             int length;
-            int output;
+            int output = 0;
             do{
-                length = Convert.ToString(sum).Length;
+                length = Convert.ToString(GV.sum).Length;
                 switch (length) 
                 {
                     case 3:
-                        output = sum / 100;
-                        dict[100] -= output;
-                        withdarw.Add(String.Format("100$ x {0}", output));
-                        sum -= output * 100;
+                        SortWithdraw(GV, output, length, new List<int> { 100 }, 100);
                         continue;
                     case 2:
-                        output = sum / 10;
-                        foreach (int i in new List<int>() { 5, 2, 1 })
-                        {
-                            if (i <= output)
-                            {
-                                dict[i * 10] -= output / i;
-                                withdarw.Add(String.Format("{0}$ x {1}", i * 10, output / i));
-                                sum -= i * 10 * (output / i);
-                                break;
-                            }
-                            else { continue; }
-                        }
+                        SortWithdraw(GV, output, length, new List<int> { 50, 20, 10 }, 10);
                         continue;
                     case 1:
-                        output = sum;
-                        foreach (int i in new List<int>() { 5, 2, 1 })
-                        {
-                            if (i <= output)
-                            {
-                                dict[i] -= output / i;
-                                withdarw.Add(String.Format("{0}$ x {1}", i, output / i));
-                                sum -= i * (output / i);
-                                break;
-                            }
-                            else { continue; }
-                        }
+                        SortWithdraw(GV, output, length, new List<int> { 5, 2, 1 }, 1);
                         continue;
                 }
-            } while (sum != 0);
-            return withdarw;
+            } while (GV.sum != 0);
         }
         public static Dictionary<int, int> CreateMoneyBank()
         {
@@ -71,10 +67,11 @@ namespace ATM_cSharp
         }
         public static void Main(string[] args)
         {
-            Dictionary<int, int> moneyDict = CreateMoneyBank();
-            int sum = InputSum();
-            List<string> withdraw = CalculateWithdraw(sum, moneyDict);
-            withdraw.ForEach(Console.WriteLine);
+            var GV = new GlobalVars();
+            GV.moneyDict = CreateMoneyBank();
+            GV.sum = InputSum();
+            CalculateWithdraw(GV);
+            GV.withdraw.ForEach(Console.WriteLine);
         }
     }
 }
